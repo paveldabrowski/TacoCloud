9b4b1a4de1fc93f3476a0ebe371a8772569031d7
paktriano <pavel_dabrowski@wp.pl>
2020-12-27 19:20:58 +0100

    chapter13 working

---
+++ ingredient-service/src/main/java/tacos/ingredients/IngredientRepository.java
@@ -0,0 +1,7 @@
+package tacos.ingredients;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*")
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {}
